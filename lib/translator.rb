5d47a34b626433eb48f8f6f5d6b61befd59462df
Steven Kandow <kando1sr@gmail.com>
2020-03-23 16:34:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,9 @@ def load_library(file)
   emoticon_library = YAML.load_file(file)
   library_sort = {"get_meaning": {}, "get_emoticon": {}}
   library_sort
-  emoticon_library_array = emoticon_library.to_a 
+  emoticon_library_array = emoticon_library.to_a
+  emoticon_library_array.each do |index|
+    meaning = index[0]
   # code goes here
 end
 
