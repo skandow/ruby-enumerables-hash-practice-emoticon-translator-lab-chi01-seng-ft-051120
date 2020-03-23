f0a4a6f5f50c23ded6af06d699d4f7548337b2fd
Steven Kandow <kando1sr@gmail.com>
2020-03-23 15:35:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,10 +6,11 @@ def load_library(file)
   emoticon_library
   library_hash = {"get_emoticon": {}, "get_meaning": {}}
   emoticon_library_sorted = emoticon_library.map do |index|
+    meaning = index
     english_emoji = index[0]
     japanese_emoji = index[1]
-    library_hash[:get_emoticon][:english_emoji] = japanese_emoji
-    library_hash[:get_meaning][:japanese_emoji] = english_emoji
+    library_hash[:get_emoticon][:"#{english_emoji}"] = japanese_emoji
+    library_hash[:get_meaning][:"#{japanese_emoji}"] = meaning
   end 
   library_hash
   # code goes here
