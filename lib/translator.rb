b6a70a165480fc1024ddf076fe0bd8356628ce40
Steven Kandow <kando1sr@gmail.com>
2020-03-23 16:05:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,6 @@ require "yaml"
 
 def load_library(file)
   emoticon_library = YAML.load_file(file)
-  emoticon_library_array = emoticon_library.to_a
   library_hash = {"get_emoticon": {}, "get_meaning": {}}
   emoticon_library_array.each do |index|
     meaning = index
