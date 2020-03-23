bd00553b6865646d1b7a524be7042f30bf2511aa
Steven Kandow <kando1sr@gmail.com>
2020-03-23 16:19:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,12 +4,15 @@ require "yaml"
 def load_library(file)
   emoticon_library = YAML.load_file(file)
   library_hash = {"get_emoticon": {}, "get_meaning": {}}
-  emoticon_library.each do |index|
-    meaning = index
-    english_emoji = index[0]
-    japanese_emoji = index[1]
-    library_hash[:get_emoticon]["#{english_emoji}"] = japanese_emoji
-    library_hash[:get_meaning]["#{japanese_emoji}"] = meaning
+  emoticon_library_array = emoticon_library.to_a
+  array_count = 0 
+  while array_count < emoticon_library_array.length do 
+    meaning = emoticon_library_array[array_count].to_s 
+    english_emoji = emoticon_library_array[array_count][0]
+    japanese_emoji = emoticon_library_array[array_count][1]
+    library_hash[:get_emoticon][english_emoji] = japanese_emoji
+    library_hash[:get_meaning][japanese_emoji] = meaning
+    
   end 
   library_hash
   # code goes here
