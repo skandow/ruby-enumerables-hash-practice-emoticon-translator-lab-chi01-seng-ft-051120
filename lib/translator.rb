6c121bae2cde896ab7a420139819b76f7d0dac30
Steven Kandow <kando1sr@gmail.com>
2020-03-23 16:37:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,6 +8,12 @@ def load_library(file)
   emoticon_library_array = emoticon_library.to_a
   emoticon_library_array.each do |index|
     meaning = index[0]
+    english_emoji = index[1][0]
+    japanese_emoji = index[1][1]
+    library_sort[:get_meaning][japanese_emoji] = meaning
+    library_sort[:get_emoticon][english_emoji] = japanese_emoji
+  end 
+  library_sort
   # code goes here
 end
 
